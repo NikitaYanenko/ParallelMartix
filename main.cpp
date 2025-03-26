3dd57c9bccfb7591558e251f78a28280eb6031fb
Mykyta Yanenko <123701722+NikitaYanenko@users.noreply.github.com>
2025-03-26 19:53:49 +0200

    Update main.cpp
    Correct timer

--- main.cpp
+++ main.cpp
@@ -9,18 +9,20 @@ int main() {
     // if used max threads;
     unsigned int numThreads = std::thread::hardware_concurrency() - 1;
     
-    const std::string firstMatrixFile = "FirstMatrix1000x1000";
-    const std::string secondMatrixFile = "SecondMatrix1000x1000";
+    const std::string firstMatrixFile = "FirstMatrix10000x10000";
+    const std::string secondMatrixFile = "SecondMatrix10000x10000";
     const std::string resultDotFile = "ResultDot";
 
     Matrix::createThreads(numThreads);
 
-    Matrix firstMatrix(1000, 1000, firstMatrixFile);
-    Matrix secondMatrix(1000, 1000, secondMatrixFile);
+    Matrix firstMatrix(10000, 10000, firstMatrixFile);
+    Matrix secondMatrix(10000, 10000, secondMatrixFile);
 
-    Timer time;
+    Timer* time = new Timer();
 
     Matrix resultDot = firstMatrix * secondMatrix;
+
+    delete time;
 
     resultDot.writeToFile(resultDotFile);
 
